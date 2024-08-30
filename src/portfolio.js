ffc782e7431c64a98640374029b54db9ff54dae2
neuronx2 <nehra.kushagra@gmail.com>
2024-08-30 14:29:00 +0200

    small changes to logo and discriptions

--- src/portfolio.js
+++ src/portfolio.js
@@ -197,8 +197,8 @@ const skills = {
       title: "Project & Product Management",
       fileName: "CloudInfraImg", //TODO:change it totally
       skills: [
-        "At various points in professional journey, I have lead various projects to end and seen development of products till the end stage.",
-        "⚡ I often act as a bridge between Clients (internal & external) and technical experters doning the hat of Project Manager or sometimes as Product Manager.",
+        "At various points in professional journey, I have lead various projects and seen development of products till the end.",
+        "⚡ I often act as a bridge between Clients (internal & external) and technical experts donning the hat of Project Manager or sometimes as Product Manager.",
         "⚡ Documenting, validating the VOC, project requirements, avoiding scope creep , allocating resources & always shooting out emails.",
         "⚡ Implimenting process for New Product Development (Blueprinting). Engaging into questions and answers to explore the hidden key deliverables.",
         "From multiple digital products to market reseach projects, my latest deliverable was a development of a physical product capable of utilization in Hydrogen Production with help of R&D team.",
@@ -541,7 +541,7 @@ const experience = {
   title: "Experience", //TODO:you can also change the name here to reflect the info
   subtitle: "Work, Internship and Volunteership",
   description:
-    "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+    "I have worked with Startups and Global enterprises alike. I have been an enterpreneur myself. As a professional who acts as a mediator between the technical professional and management decision makers, I love love solving business strategy challenges that are identified based on data and their solution lies mostly in what data says or soemtimes outside it.",
   header_image_path: "experience.svg",
   sections: [
     {
@@ -549,47 +549,64 @@ const experience = {
       work: true,
       experiences: [
         {
-          title: "Machine Learning Engineer",
-          company: "TikTok Inc.",
-          company_url: "https://www.tiktok.com/en/",
-          logo_path: "tiktok_logo.png",
-          duration: "June 2023 - Present",
-          location: "San Jose, CA, USA",
+          title: "Manager New Market Development",
+          company: "Morgan Advanced Materials Plc.",
+          company_url: "https://www.morganadvancedmaterials.com/en-gb/",
+          logo_path: "mam_logo.png",
+          duration: "March 2022 - Present",
+          location: "Berlin, Germany",
           description:
-            "Improving ads ranking models on the core TikTok product. Experience working on modeling two-tower architectures like DeepFM, Wide & deep learning, etc. Working on Large Language Models (LLM) pretraining and Large Multi-modal Model (LMM) finetuning strategies.",
+            "It started with an Interview as BI head for Performance Carbon SBU and moved into Strategic role to assist Board members in Innovation and New Market Development at Morgan",
           color: "#000000",
         },
         {
-          title: "Associate AI Engineer",
-          company: "Legato Health Technology",
-          company_url: "https://legatohealthtech.com/",
-          logo_path: "legato_logo.png",
-          duration: "June 2020 - Aug 2021",
-          location: "Hyderabad, Telangana",
-          description:
-            "I am working on automating healthcare products. The projects involve automation for process improvements and for significantly enhancing the profits. I am currently working on Cancer Survival and Reoccurence Prediction. Our goal is to make AI system which scales and removes doctor dependency as much as possible.",
+          title: "Senior Financial Analyst Marketing BI ",
+          company: "Hellofresh SE",
+          company_url: "https://www.hellofresh.de/",
+          logo_path: "hf_logo.png",
+          duration: "April 2020 - Feb 2022",
+          location: "Berlin, Germany",
+          description: "____",
           color: "#0879bf",
         },
         {
-          title: "Android and ML Developer",
-          company: "Muffito Incorporation",
-          company_url: "https://www.linkedin.com/company/muffito-inc/about/",
-          logo_path: "muffito_logo.png",
-          duration: "May 2018 - Oct 2018",
-          location: "Pune, Maharashtra",
-          description:
-            "I have created complete Android Application for locating Pub, Bar and beverage shops around you. I have also worked on implementation of algorithms for Face Detection, Text extraction from Image. I was involved in a team for creating complete software architecure of mobile and web application as well as admin panel for company.",
+          title: "Manager Global Business Operation",
+          company: "Toluna Inc.",
+          company_url: "https://www.toluna.com/",
+          logo_path: "toluna_logo.png",
+          duration: "May 2015 - Aug 2018",
+          location: "Gurugram,India",
+          description: "____",
           color: "#9b1578",
         },
         {
-          title: "Android Developer",
-          company: "FreeCopy Pvt. Ltd.",
-          company_url: "https://www.linkedin.com/company/freecopy/about/",
-          logo_path: "freecopy_logo.png",
+          title: "Financial Analyst FP&A",
+          company: "Dynata Inc.",
+          company_url: "https://www.dynata.com/",
+          logo_path: "dynata_logo.png",
           duration: "Nov 2017 - Dec 2017",
-          location: "Ahmedabad, Gujarat",
-          description:
-            "FreeCopy is the Start up from Indian Institute of Management, Ahmedabad. I have changed the integration of the whole app from Google to Firebase. I learnt the efﬁcient ways of Data communications like Retroﬁt, Eventbus etc. I experienced the real time start up. I learnt the Design thinking of UI on perspective of People.",
+          location: "New Delhi, India",
+          description: "____",
+          color: "#fc1f20",
+        },
+        {
+          title: "Project Manager",
+          company: "Dynata Inc.",
+          company_url: "https://www.dynata.com/",
+          logo_path: "dynata_logo.png",
+          duration: "Nov 2017 - Dec 2017",
+          location: "New Delhi, India",
+          description: "____",
+          color: "#fc1f20",
+        },
+        {
+          title: "Assistant Manager",
+          company: "GE Capital",
+          company_url: "https://www.gecapital.com/",
+          logo_path: "ge_logo.png",
+          duration: "Nov 2017 - Dec 2017",
+          location: "Lucknow, India",
+          description: "____",
           color: "#fc1f20",
         },
       ],
@@ -598,42 +615,39 @@ const experience = {
       title: "Internships", //TODO: internships
       experiences: [
         {
-          title: "Machine Learning Intern",
-          company: "TikTok Inc.",
-          company_url: "https://www.tiktok.com/en/",
-          logo_path: "tiktok_logo.png",
-          duration: "May 2022 - Aug 2022",
-          location: "San Francisco, USA",
-          description:
-            "Building new features on the backend recommendation system, specifically ranking algorithms for Ads that touch hundreds of millions of people around the world. Improving online and offline content ranking algorithms by performing hard sample data replays for training steps.",
+          title: "Intern Vaccine Research",
+          company: "Talwar Research Foundation",
+          company_url:
+            "https://www.indiascienceandtechnology.gov.in/allstinstitutions/talwar-research-foundation",
+          logo_path: "trf_logo.png",
+          duration: "May 2005 - Aug 2005",
+          location: "New Delhi, India",
+          description: "____",
           color: "#000000",
         },
         {
-          title: "Data Science Research Intern",
-          company: "Delhivery Pvt. Ltd.",
-          company_url: "https://www.delhivery.com/",
-          logo_path: "delhivery_logo.png",
-          duration: "May 2019 - Sept 2019",
-          location: "Gurgaon, Haryana",
-          description:
-            "I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
+          title: "Mutual Funds Investment",
+          company: "ICICI Prudential Asset Management Company",
+          company_url: "https://www.icicipruamc.com/",
+          logo_path: "icicipru_logo.png",
+          duration: "May 2007 - Aug 2007",
+          location: "New Delhi, India",
+          description: "____",
           color: "#ee3c26",
         },
         {
-          title: "Data Science Intern",
-          company: "Intel Indexer LLC",
-          company_url:
-            "https://opencorporates.com/companies/us_dc/EXTUID_4170286",
-          logo_path: "intel_logo.jpg",
-          duration: "Nov 2018 - Dec 2018",
-          location: "Work From Home",
-          description:
-            "This is financial Solution Company. I have made Supervised Learning model for the company which can perform time series analysis on Stock price data for 32 companies. I have built LSTM Neural Networks Model and trained the data of 32 companies for last 2 years. This model is also used for forecasting.",
+          title: "Data Analytics Intern",
+          company: "Ommax GmBH",
+          company_url: "https://www.ommax-digital.com/en/",
+          logo_path: "ommax_logo.png",
+          duration: "Aug 2019 - Jan 2020",
+          location: "Munich, Germany",
+          description: "____",
           color: "#0071C5",
         },
       ],
     },
-    {
+    /*{
       title: "Volunteerships", //TODO:social work
       experiences: [
         {
@@ -693,7 +707,7 @@ const experience = {
           color: "#181717",
         },
       ],
-    },
+    },*/
   ],
 };
 
